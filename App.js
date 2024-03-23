7ee4cee1b4ec5bbc15f70eefd55c6bd03cec7aa7
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-03-23 18:36:59 +0530

    added flatcard

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import FlatCards from './component/FlatCards';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <ScrollView>
+    
+      <Text>Hi</Text>
+     
+    <FlatCards/>
+    
+    </ScrollView>
+    </SafeAreaView>
   );
 }
 
